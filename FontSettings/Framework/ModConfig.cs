42ae9f79c60e4c02158ff4e3983fb2598606cf9f
Becks723 <none>
2022-07-24 02:04:19 +0800

    配置的属性重新排列。

--- FontSettings/Framework/ModConfig.cs
+++ FontSettings/Framework/ModConfig.cs
@@ -6,6 +6,18 @@
 
         internal FontConfigs Fonts { get; set; }  // 这个不要储存在config.json里，故设为internal。
 
+        public int MinFontSize { get; } = 1;
+
+        public int MaxFontSize { get; set; } = 100;
+
+        public int MinSpacing { get; set; } = -10;
+
+        public int MaxSpacing { get; set; } = 10;
+
+        public int MinLineSpacing { get; } = 1;
+
+        public int MaxLineSpacing { get; set; } = 100;
+
         public int MinCharOffsetX { get; set; } = -10;
 
         public int MaxCharOffsetX { get; set; } = 10;
@@ -14,30 +26,18 @@
 
         public int MaxCharOffsetY { get; set; } = 10;
 
-        public int MaxFontSize { get; set; } = 100;
-
-        public int MinFontSize { get; } = 1;
-
-        public int MaxSpacing { get; set; } = 10;
-
-        public int MinSpacing { get; set; } = -10;
-
-        public int MaxLineSpacing { get; set; } = 100;
-
-        public int MinLineSpacing { get; } = 1;
-
         public void ResetToDefault()
         {
             this.ExampleText = "AaBbYyZz\n测试用例";
             this.Fonts = new FontConfigs();
             this.MaxFontSize = 100;
-            this.MaxSpacing = 10;
             this.MinSpacing = -10;
+            this.MaxSpacing = 10;
             this.MaxLineSpacing = 100;
-            this.MaxCharOffsetX = 10;
             this.MinCharOffsetX = -10;
-            this.MaxCharOffsetY = 10;
+            this.MaxCharOffsetX = 10;
             this.MinCharOffsetY = -10;
+            this.MaxCharOffsetY = 10;
         }
     }
 }
