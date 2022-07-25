f60bb38e81c975d7db053d6d68c3958bc23d1683
Becks723 <none>
2022-07-25 19:48:50 +0800

    界面添加刷新按钮。

---
+++ FontSettings/Framework/Menus/RefreshButton.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValleyUI.Controls;
+
+namespace FontSettings.Framework.Menus
+{
+    internal class RefreshButton : TextureButton
+    {
+        private static readonly Lazy<Texture2D> _refreshTexture = new(LoadRefreshTexture);
+
+        private bool _isAnimating;
+
+        private double _angle;
+
+        /// <summary>动画一次全程经过的时间，单位为毫秒。</summary>
+        public int AnimationDuration { get; set; } = 500;
+
+        /// <summary>动画一次全程转过的角度，如360表示360°，转一圈。</summary>
+        public int RotationAngle { get; set; } = 180;
+
+        public RefreshButton(float scale)
+            : base(_refreshTexture.Value, null, scale)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (this._isAnimating)
+            {
+                this._angle += this.GetAngleDeltaPerUpdate();
+                if (this._angle > this.RotationAngle)
+                {
+                    this._angle = 0;
+                    this._isAnimating = false;
+                }
+            }
+        }
+
+        public override void Draw(SpriteBatch b)
+        {
+            if (this.Texture != null)
+            {
+                int texturewidth = this.SourceRectangle.HasValue ? this.SourceRectangle.Value.Width : this.Texture.Width;
+                int textureHeight = this.SourceRectangle.HasValue ? this.SourceRectangle.Value.Height : this.Texture.Height;
+                b.Draw(this.Texture, this.Position + new Vector2(texturewidth / 2, textureHeight / 2) * this.BaseScale, this.SourceRectangle, Color.White * (this.GreyedOut ? 0.33f : 1f), this.CalculateRotationByAngle(this._angle), new Vector2(texturewidth / 2, textureHeight / 2), this.Scale, SpriteEffects.None, 0f);
+            }
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+
+            if (!this._isAnimating)
+            {
+                this._isAnimating = true;
+                this._angle = 0;
+            }
+        }
+
+        private double GetAngleDeltaPerUpdate()
+        {
+            const int fpsRate = 60;
+            double count = this.AnimationDuration / 1000.0 * fpsRate;
+            return this.RotationAngle / count;
+        }
+
+        private float CalculateRotationByAngle(double angle)
+        {
+            double unit = Math.PI / 180;
+            return (float)(unit * angle);
+        }
+
+        private static Texture2D LoadRefreshTexture()
+        {
+            return ModEntry.ModHelper.ModContent.Load<Texture2D>("assets/refresh.png");
+        }
+    }
+}
