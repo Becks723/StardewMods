eb08d7250178c2f199ae99c74a5b9495fa7e5abb
Becks723 <Becks723@DESKTOP-2138RE9>
2022-09-04 17:40:14 +0800

    添加对鼠标悬浮文本的阴影修复。

--- FontSettings/Framework/Patchers/FontShadowPatcher.cs
+++ FontSettings/Framework/Patchers/FontShadowPatcher.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using HarmonyLib;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.Menus;
 
 namespace FontSettings.Framework.Patchers
 {
@@ -34,6 +34,11 @@ namespace FontSettings.Framework.Patchers
                 original: AccessTools.Method(typeof(Utility), nameof(Utility.drawTextWithColoredShadow)),
                 prefix: new HarmonyMethod(typeof(FontShadowPatcher), nameof(Utility_drawTextWithColoredShadow_Prefix))
             );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(IClickableMenu), nameof(IClickableMenu.drawHoverText), new[] { typeof(SpriteBatch), typeof(StringBuilder), typeof(SpriteFont), typeof(int), typeof(int), typeof(int), typeof(string), typeof(int), typeof(string[]), typeof(Item), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(float), typeof(CraftingRecipe), typeof(IList<Item>) }),
+                prefix: new HarmonyMethod(typeof(FontShadowPatcher), nameof(IClickableMenu_drawHoverText_Prefix)),
+                postfix: new HarmonyMethod(typeof(FontShadowPatcher), nameof(IClickableMenu_drawHoverText_Postfix))
+            );
         }
 
         private static void Utility_drawTextWithShadow_Prefix(ref float shadowIntensity)
@@ -46,6 +51,21 @@ namespace FontSettings.Framework.Patchers
         {
             if (_config.DisableTextShadow)
                 shadowColor = Color.Transparent;
+        }
+
+        private static void IClickableMenu_drawHoverText_Prefix(out Color __state)  // __state里记录了原来的阴影颜色
+        {
+            __state = Game1.textShadowColor;
+
+            // 在方法开始前，将阴影颜色设置为透明。
+            if (_config.DisableTextShadow)
+                Game1.textShadowColor = Color.Transparent;
+        }
+
+        private static void IClickableMenu_drawHoverText_Postfix(Color __state)
+        {
+            // 在方法结束后，恢复原来的阴影颜色。
+            Game1.textShadowColor = __state;
         }
     }
 }
