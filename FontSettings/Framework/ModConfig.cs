240bef8409c6da43cc6f258418e94246546649e8
Becks723 <Becks723@DESKTOP-2138RE9>
2022-12-25 19:05:51 +0800

    优化配置文件： 1. 添加MinFontSize、MinLineSpacing供配置。 2. 优化默认值，不至于过大过小。 3. ExampleText默认值改为空字符串。

--- FontSettings/Framework/ModConfig.cs
+++ FontSettings/Framework/ModConfig.cs
@@ -5,35 +5,35 @@ namespace FontSettings.Framework
 {
     internal class ModConfig
     {
-        public string ExampleText { get; set; } = "AaBbYyZz\n测试用例";
+        public string ExampleText { get; set; }
 
         internal FontConfigs Fonts { get; set; }  // 这个不要储存在config.json里，故设为internal。
 
         internal SampleData Sample { get; set; }  // 同上
 
-        internal int MinFontSize { get; } = 1;  // 只读属性pulic的话会显示在json文件里。
+        public int MinFontSize { get; set; }
 
-        public int MaxFontSize { get; set; } = 100;
+        public int MaxFontSize { get; set; }
 
-        public int MinSpacing { get; set; } = -10;
+        public int MinSpacing { get; set; }
 
-        public int MaxSpacing { get; set; } = 10;
+        public int MaxSpacing { get; set; }
 
-        internal int MinLineSpacing { get; } = 1;
+        public int MinLineSpacing { get; set; }
 
-        public int MaxLineSpacing { get; set; } = 100;
+        public int MaxLineSpacing { get; set; }
 
-        public int MinCharOffsetX { get; set; } = -10;
+        public int MinCharOffsetX { get; set; }
 
-        public int MaxCharOffsetX { get; set; } = 10;
+        public int MaxCharOffsetX { get; set; }
 
-        public int MinCharOffsetY { get; set; } = -10;
+        public int MinCharOffsetY { get; set; }
 
-        public int MaxCharOffsetY { get; set; } = 10;
+        public int MaxCharOffsetY { get; set; }
 
-        public float MinPixelZoom { get; set; } = 0.1f;
+        public float MinPixelZoom { get; set; }
 
-        public float MaxPixelZoom { get; set; } = 5f;
+        public float MaxPixelZoom { get; set; }
 
         public bool DisableTextShadow { get; set; } = false;
 
@@ -41,18 +41,40 @@ namespace FontSettings.Framework
 
         public bool FontSettingsInGameMenu { get; set; } = true;
 
+        private readonly int DEFAULT_MinFontSize = 5;
+        private readonly int DEFAULT_MaxFontSize = 75;
+        private readonly int DEFAULT_MinSpacing = -10;
+        private readonly int DEFAULT_MaxSpacing = 10;
+        private readonly int DEFAULT_MinLineSpacing = 5;
+        private readonly int DEFAULT_MaxLineSpacing = 75;
+        private readonly int DEFAULT_MinCharOffsetX = -10;
+        private readonly int DEFAULT_MaxCharOffsetX = 10;
+        private readonly int DEFAULT_MinCharOffsetY = -10;
+        private readonly int DEFAULT_MaxCharOffsetY = 10;
+        private readonly float DEFAULT_MinPixelZoom = 0.5f;
+        private readonly float DEFAULT_MaxPixelZoom = 5f;
+
+        public ModConfig()
+        {
+            this.ResetToDefault();
+        }
+
         public void ResetToDefault()
         {
-            this.ExampleText = "AaBbYyZz\n测试用例";
+            this.ExampleText = string.Empty;
             this.Fonts = new FontConfigs();
-            this.MaxFontSize = 100;
-            this.MinSpacing = -10;
-            this.MaxSpacing = 10;
-            this.MaxLineSpacing = 100;
-            this.MinCharOffsetX = -10;
-            this.MaxCharOffsetX = 10;
-            this.MinCharOffsetY = -10;
-            this.MaxCharOffsetY = 10;
+            this.MinFontSize = this.DEFAULT_MinFontSize;
+            this.MaxFontSize = this.DEFAULT_MaxFontSize;
+            this.MinSpacing = this.DEFAULT_MinSpacing;
+            this.MaxSpacing = this.DEFAULT_MaxSpacing;
+            this.MinLineSpacing = this.DEFAULT_MinLineSpacing;
+            this.MaxLineSpacing = this.DEFAULT_MaxLineSpacing;
+            this.MinCharOffsetX = this.DEFAULT_MinCharOffsetX;
+            this.MaxCharOffsetX = this.DEFAULT_MaxCharOffsetX;
+            this.MinCharOffsetY = this.DEFAULT_MinCharOffsetY;
+            this.MaxCharOffsetY = this.DEFAULT_MaxCharOffsetY;
+            this.MinPixelZoom = this.DEFAULT_MinPixelZoom;
+            this.MaxPixelZoom = this.DEFAULT_MaxPixelZoom;
             this.DisableTextShadow = false;
             this.OpenFontSettingsMenu = KeybindList.Parse($"{nameof(SButton.LeftAlt)} + {nameof(SButton.F)}");
             this.FontSettingsInGameMenu = true;
