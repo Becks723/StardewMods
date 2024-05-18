d5c6c7347ffdf2eecb6861dcb442779f980f7a58
Becks723 <1991737469@qq.com>
2024-05-18 16:23:50 +0800

    重构GMCM菜单：使其能在不同语言下显示不同选项。

--- FontSettings/Framework/Integrations/GMCMIntegration.cs
+++ FontSettings/Framework/Integrations/GMCMIntegration.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using CodeShared.Integrations.GenericModConfigMenu;
 using StardewModdingAPI;
+using StardewValley;
 
 namespace FontSettings.Framework.Integrations
 {
     internal class GMCMIntegration : GenericModConfigMenuIntegrationBase
     {
+        private GenericModConfigMenuFluentHelper _helperCache;
+
         private ModConfig Config { get; }
 
         public GMCMIntegration(ModConfig config, Action reset, Action save, IModRegistry modRegistry, IMonitor monitor, IManifest manifest, bool isGMCMOptionsRequired = false)
@@ -15,6 +18,20 @@ namespace FontSettings.Framework.Integrations
         }
 
         protected override void IntegrateOverride(GenericModConfigMenuFluentHelper helper)
+        {
+            LocalizedContentManager.OnLanguageChange += this.OnGameLanguageChanged;
+
+            this._helperCache = helper;
+            this.Register(this._helperCache, LocalizedContentManager.CurrentLanguageCode);
+        }
+
+        private void OnGameLanguageChanged(LocalizedContentManager.LanguageCode code)
+        {
+            this._helperCache.Unregister();
+            this.Register(this._helperCache, code);
+        }
+
+        private void Register(GenericModConfigMenuFluentHelper helper, LocalizedContentManager.LanguageCode language)
         {
             helper.Register()
                 // color & shadow section
@@ -60,187 +77,192 @@ namespace FontSettings.Framework.Integrations
                 )
 
                 // compatibility section
-                .AddSectionTitle(I18n.Config_SectionCompatibility)
+                .AddSectionTitle(I18n.Config_SectionCompatibility);
 
+            if (FontHelpers.IsCurrentLatinLanguage())
+            {
                 // enable latin dialogue font
-                .AddCheckbox(
+                //   (only show if unde latin language)
+                helper.AddCheckbox(
                     name: I18n.Config_EnableLatinDialogueFont,
                     tooltip: () => I18n.Config_EnableLatinDialogueFont_Description(
                         locale: FontHelpers.GetCurrentDisplayLocale(),
                         isLatinLanguage: FontHelpers.IsCurrentLatinLanguage()),
                     get: () => this.Config.EnableLatinDialogueFont,
                     set: val => this.Config.EnableLatinDialogueFont = val
-                )
-
-                // edit mode
-                .AddCheckbox(
-                    name: I18n.Config_EditMode,
-                    tooltip: I18n.Config_EditMode_Description,
-                    get: () => this.Config.EditMode,
-                    set: val => this.Config.EditMode = val
-                )
-
-                // edit priority
-                .AddSlider(
-                    name: I18n.Config_EditPrio,
-                    tooltip: () => I18n.Config_EditPrio_Description(int.MinValue, int.MaxValue),
-                    get: () => this.Config.EditPriority,
-                    set: val => this.Config.EditPriority = val,
-                    max: null,
-                    min: null,     // null for allow any integer.
-                    interval: null
-                )
-
-                // ui section
-                .AddSectionTitle(I18n.Config_SectionUi)
-
-                // menu key
-                .AddKeyBindList(
-                    name: I18n.Config_FontMenuKey,
-                    tooltip: I18n.Config_FontMenuKey_Description,
-                    get: () => this.Config.OpenFontSettingsMenu,
-                    set: val => this.Config.OpenFontSettingsMenu = val
-                )
-
-                // sample text
-                .AddTextBox(
-                    name: I18n.Config_ExampleText,
-                    tooltip: I18n.Config_ExampleText_Description,
-                    get: () => NormalizeExampleText(this.Config.ExampleText),
-                    set: val => this.Config.ExampleText = ParseBackExampleText(val)
-                )
-
-                // min font size
-                .AddSlider(
-                    name: I18n.Config_MinFontSize,
-                    tooltip: I18n.Config_MinFontSize_Description,
-                    get: () => this.Config.MinFontSize,
-                    set: val => this.Config.MinFontSize = val,
-                    max: 30,
-                    min: 1,
-                    interval: 1
-                )
-
-                // max font size
-                .AddSlider(
-                    name: I18n.Config_MaxFontSize,
-                    tooltip: I18n.Config_MaxFontSize_Description,
-                    get: () => this.Config.MaxFontSize,
-                    set: val => this.Config.MaxFontSize = val,
-                    max: 150,
-                    min: 50,
-                    interval: 1
-                )
-
-                // min spacing
-                .AddSlider(
-                    name: I18n.Config_MinSpacing,
-                    tooltip: I18n.Config_MinSpacing_Description,
-                    get: () => this.Config.MinSpacing,
-                    set: val => this.Config.MinSpacing = val,
-                    max: -5,
-                    min: -20,
-                    interval: 1
-                )
-
-                // max spacing
-                .AddSlider(
-                    name: I18n.Config_MaxSpacing,
-                    tooltip: I18n.Config_MaxSpacing_Description,
-                    get: () => this.Config.MaxSpacing,
-                    set: val => this.Config.MaxSpacing = val,
-                    max: 20,
-                    min: 5,
-                    interval: 1
-                )
-
-                // min line spacing
-                .AddSlider(
-                    name: I18n.Config_MinLineSpacing,
-                    tooltip: I18n.Config_MinLineSpacing_Description,
-                    get: () => this.Config.MinLineSpacing,
-                    set: val => this.Config.MinLineSpacing = val,
-                    max: 30,
-                    min: 1,
-                    interval: 1
-                )
-
-                // max line spacing
-                .AddSlider(
-                    name: I18n.Config_MaxLineSpacing,
-                    tooltip: I18n.Config_MaxLineSpacing_Description,
-                    get: () => this.Config.MaxLineSpacing,
-                    set: val => this.Config.MaxLineSpacing = val,
-                    max: 150,
-                    min: 50,
-                    interval: 1
-                )
-
-                // min x offset
-                .AddSlider(
-                    name: I18n.Config_MinOffsetX,
-                    tooltip: I18n.Config_MinOffsetX_Description,
-                    get: () => this.Config.MinCharOffsetX,
-                    set: val => this.Config.MinCharOffsetX = val,
-                    max: -10,
-                    min: -200,
-                    interval: 1
-                )
-
-                // max x offset
-                .AddSlider(
-                    name: I18n.Config_MaxOffsetX,
-                    tooltip: I18n.Config_MaxOffsetX_Description,
-                    get: () => this.Config.MaxCharOffsetX,
-                    set: val => this.Config.MaxCharOffsetX = val,
-                    max: 200,
-                    min: 10,
-                    interval: 1
-                )
-
-                // min y offset
-                .AddSlider(
-                    name: I18n.Config_MinOffsetY,
-                    tooltip: I18n.Config_MinOffsetY_Description,
-                    get: () => this.Config.MinCharOffsetY,
-                    set: val => this.Config.MinCharOffsetY = val,
-                    max: -10,
-                    min: -200,
-                    interval: 1
-                )
-
-                // max y offset
-                .AddSlider(
-                    name: I18n.Config_MaxOffsetY,
-                    tooltip: I18n.Config_MaxOffsetY_Description,
-                    get: () => this.Config.MaxCharOffsetY,
-                    set: val => this.Config.MaxCharOffsetY = val,
-                    max: 200,
-                    min: 10,
-                    interval: 1
-                )
-
-                // min pixel zoom
-                .AddSlider(
-                    name: I18n.Config_MinPixelZoom,
-                    tooltip: I18n.Config_MinPixelZoom_Description,
-                    get: () => this.Config.MinPixelZoom,
-                    set: val => this.Config.MinPixelZoom = val,
-                    max: 3,
-                    min: 0.1f,
-                    interval: 0.05f
-                )
-
-                // max pixel zoom
-                .AddSlider(
-                    name: I18n.Config_MaxPixelZoom,
-                    tooltip: I18n.Config_MaxPixelZoom_Description,
-                    get: () => this.Config.MaxPixelZoom,
-                    set: val => this.Config.MaxPixelZoom = val,
-                    max: 10,
-                    min: 3,
-                    interval: 0.05f
                 );
+            }
+
+
+            // edit mode
+            helper.AddCheckbox(
+                name: I18n.Config_EditMode,
+                tooltip: I18n.Config_EditMode_Description,
+                get: () => this.Config.EditMode,
+                set: val => this.Config.EditMode = val
+            )
+
+            // edit priority
+            .AddSlider(
+                name: I18n.Config_EditPrio,
+                tooltip: () => I18n.Config_EditPrio_Description(int.MinValue, int.MaxValue),
+                get: () => this.Config.EditPriority,
+                set: val => this.Config.EditPriority = val,
+                max: null,
+                min: null,     // null for allow any integer.
+                interval: null
+            )
+
+            // ui section
+            .AddSectionTitle(I18n.Config_SectionUi)
+
+            // menu key
+            .AddKeyBindList(
+                name: I18n.Config_FontMenuKey,
+                tooltip: I18n.Config_FontMenuKey_Description,
+                get: () => this.Config.OpenFontSettingsMenu,
+                set: val => this.Config.OpenFontSettingsMenu = val
+            )
+
+            // sample text
+            .AddTextBox(
+                name: I18n.Config_ExampleText,
+                tooltip: I18n.Config_ExampleText_Description,
+                get: () => NormalizeExampleText(this.Config.ExampleText),
+                set: val => this.Config.ExampleText = ParseBackExampleText(val)
+            )
+
+            // min font size
+            .AddSlider(
+                name: I18n.Config_MinFontSize,
+                tooltip: I18n.Config_MinFontSize_Description,
+                get: () => this.Config.MinFontSize,
+                set: val => this.Config.MinFontSize = val,
+                max: 30,
+                min: 1,
+                interval: 1
+            )
+
+            // max font size
+            .AddSlider(
+                name: I18n.Config_MaxFontSize,
+                tooltip: I18n.Config_MaxFontSize_Description,
+                get: () => this.Config.MaxFontSize,
+                set: val => this.Config.MaxFontSize = val,
+                max: 150,
+                min: 50,
+                interval: 1
+            )
+
+            // min spacing
+            .AddSlider(
+                name: I18n.Config_MinSpacing,
+                tooltip: I18n.Config_MinSpacing_Description,
+                get: () => this.Config.MinSpacing,
+                set: val => this.Config.MinSpacing = val,
+                max: -5,
+                min: -20,
+                interval: 1
+            )
+
+            // max spacing
+            .AddSlider(
+                name: I18n.Config_MaxSpacing,
+                tooltip: I18n.Config_MaxSpacing_Description,
+                get: () => this.Config.MaxSpacing,
+                set: val => this.Config.MaxSpacing = val,
+                max: 20,
+                min: 5,
+                interval: 1
+            )
+
+            // min line spacing
+            .AddSlider(
+                name: I18n.Config_MinLineSpacing,
+                tooltip: I18n.Config_MinLineSpacing_Description,
+                get: () => this.Config.MinLineSpacing,
+                set: val => this.Config.MinLineSpacing = val,
+                max: 30,
+                min: 1,
+                interval: 1
+            )
+
+            // max line spacing
+            .AddSlider(
+                name: I18n.Config_MaxLineSpacing,
+                tooltip: I18n.Config_MaxLineSpacing_Description,
+                get: () => this.Config.MaxLineSpacing,
+                set: val => this.Config.MaxLineSpacing = val,
+                max: 150,
+                min: 50,
+                interval: 1
+            )
+
+            // min x offset
+            .AddSlider(
+                name: I18n.Config_MinOffsetX,
+                tooltip: I18n.Config_MinOffsetX_Description,
+                get: () => this.Config.MinCharOffsetX,
+                set: val => this.Config.MinCharOffsetX = val,
+                max: -10,
+                min: -200,
+                interval: 1
+            )
+
+            // max x offset
+            .AddSlider(
+                name: I18n.Config_MaxOffsetX,
+                tooltip: I18n.Config_MaxOffsetX_Description,
+                get: () => this.Config.MaxCharOffsetX,
+                set: val => this.Config.MaxCharOffsetX = val,
+                max: 200,
+                min: 10,
+                interval: 1
+            )
+
+            // min y offset
+            .AddSlider(
+                name: I18n.Config_MinOffsetY,
+                tooltip: I18n.Config_MinOffsetY_Description,
+                get: () => this.Config.MinCharOffsetY,
+                set: val => this.Config.MinCharOffsetY = val,
+                max: -10,
+                min: -200,
+                interval: 1
+            )
+
+            // max y offset
+            .AddSlider(
+                name: I18n.Config_MaxOffsetY,
+                tooltip: I18n.Config_MaxOffsetY_Description,
+                get: () => this.Config.MaxCharOffsetY,
+                set: val => this.Config.MaxCharOffsetY = val,
+                max: 200,
+                min: 10,
+                interval: 1
+            )
+
+            // min pixel zoom
+            .AddSlider(
+                name: I18n.Config_MinPixelZoom,
+                tooltip: I18n.Config_MinPixelZoom_Description,
+                get: () => this.Config.MinPixelZoom,
+                set: val => this.Config.MinPixelZoom = val,
+                max: 3,
+                min: 0.1f,
+                interval: 0.05f
+            )
+
+            // max pixel zoom
+            .AddSlider(
+                name: I18n.Config_MaxPixelZoom,
+                tooltip: I18n.Config_MaxPixelZoom_Description,
+                get: () => this.Config.MaxPixelZoom,
+                set: val => this.Config.MaxPixelZoom = val,
+                max: 10,
+                min: 3,
+                interval: 0.05f
+            );
         }
 
         private static string NormalizeExampleText(string value)
